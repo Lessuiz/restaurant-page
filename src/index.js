5d4cef34d71ba3fd7129e13be1461a5d1ef8d52b
lessuiz <leonardoparreira2018@gmail.com>
2020-10-20 16:12:12 -0300

    finished contact

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import main from "./main-page"
 import menu from './menu-page'
+import contact from './contact-page'
 
 const mainTab = document.querySelector('#main-button')
 const menuTab = document.querySelector('#menu-button')
+const contactTab = document.querySelector('#contact-button')
 
 main()
 
 mainTab.addEventListener('click', main)
+contactTab.addEventListener('click', contact)
 menuTab.addEventListener('click', menu)
