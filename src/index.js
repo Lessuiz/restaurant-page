e0e7b5e38771d83ad6361bd8cf5cdce53ebd02c6
lessuiz <leonardoparreira2018@gmail.com>
2020-10-20 03:04:15 -0300

    finished menu page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import main from "./main-page"
+import menu from './menu-page'
 
 const mainTab = document.querySelector('#main-button')
+const menuTab = document.querySelector('#menu-button')
 
 main()
 
 mainTab.addEventListener('click', main)
+menuTab.addEventListener('click', menu)
